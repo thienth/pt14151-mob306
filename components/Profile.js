cd659b8ccd28ff81360878c1397699ef9bf7874e
thienth32 <thienth32@gmail.com>
2019-05-22 13:20:35 +0700

    update

---
+++ components/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class Profile extends React.Component {
+    constructor(props){
+        super(props);
+
+        
+    }
+
+    render(){
+        return (
+            <View >
+                <Text>Screen profile</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+   
+});
